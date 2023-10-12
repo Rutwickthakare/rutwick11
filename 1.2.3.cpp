2d1c8146c0869cadb320de5d49b7bdb86e292321
Rutwickthakare <147582744+Rutwickthakare@users.noreply.github.com>
2023-10-12 10:22:22 +0530

    Add files via upload

---
+++ 1.2.3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Even numbers from 1 to 20:" << endl;
+
+    for (int i = 2; i <= 20; i += 2) {
+        cout << i << " ";
+    }
+
+   cout << endl;
+
+    return 0;
+}
